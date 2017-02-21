55416480656118d378eb046a31399dbdd21f8d95
Ed Singleton <singletoned@gmail.com>
2017-02-21 14:30:48 +0000

    Parse json

--- scraper/spiders/odata.py
+++ scraper/spiders/odata.py
@@ -1,13 +1,9 @@
 # -*- coding: utf-8 -*-
+import json
+
 import scrapy
-from lxml import etree
 
 from scraper import settings
-
-
-def is_xml(response):
-    if b"application/xml" in response.headers['Content-Type']:
-        return True
 
 
 class OdataSpider(scrapy.Spider):
@@ -16,28 +12,8 @@ class OdataSpider(scrapy.Spider):
     start_urls = settings.START_URLS
 
     def parse(self, response):
-        if is_xml(response):
-            root = etree.fromstring(response.body)
-            print(root)
-
-            links = root.xpath('''//*[@href]''')
-            print(links)
-
-            for link in links:
-                print(response.urljoin(link.attrib["href"]))
-                yield scrapy.Request(response.urljoin(link.attrib["href"]))
-
-        yield None
-
-    #     for href in response.css("a::attr('href')").extract():
-    #         if href.startswith("/report/"):
-    #             url = response.urljoin(href)
-    #             yield scrapy.Request(url, callback=self.parse_match)
-    #     for href in response.css("option::attr('value')").extract():
-    #         if href.startswith("/schedule/"):
-    #             url = response.urljoin(href)
-    #             yield scrapy.Request(url)
-
-    # def parse_match(self, response):
-    #     data = collect_tables(response)
-    #     yield data
+        if response.url.strip("/").endswith(".svc"):
+            data = json.loads(response.body)
+            items = data['value']
+            for item in items:
+                yield scrapy.Request(response.urljoin(item['url']))
