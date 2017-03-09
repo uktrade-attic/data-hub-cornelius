2e5c82571dc0d2458e11ea3cb855b86901b27057
Ed Singleton <singletoned@gmail.com>
2017-03-09 10:27:31 +0000

    Update collect script

--- scraper/collect.py
+++ scraper/collect.py
@@ -1,60 +1,67 @@
 # -*- coding: utf-8 -*-
 
 import os
-import tempfile
 import json
 import functools
 
-import boto3
+# import boto3
 
-import settings, storage
+# import settings
+# import storage
+
+__here__ = os.path.dirname(__file__)
+INPUT_DIR = os.path.join(__here__, "..", "cache", "CACHE", "XRMServices", "2011")
+OUTPUT_DIR = os.path.join(__here__, "..", "output")
 
 
-def get_objects(bucket):
-    for key in bucket.objects.all():
-        if key.key.strip("/").startswith(storage.cache_key_prefix):
-            text = storage.get_s3_text(bucket, key.key)
-            yield text
+# def get_objects(bucket):
+#     for key in bucket.objects.all():
+#         if key.key.strip("/").startswith(storage.cache_key_prefix):
+#             text = storage.get_s3_text(bucket, key.key)
+#             yield text
 
-def get_bucket():
-    bucket_name = settings.S3CACHE_BUCKET
-    assert bucket_name, "No bucket configured"
-    s3 = boto3.resource('s3')
-    bucket = s3.Bucket(bucket_name)
-    return bucket
+# def get_bucket():
+#     bucket_name = settings.S3CACHE_BUCKET
+#     assert bucket_name, "No bucket configured"
+#     s3 = boto3.resource('s3')
+#     bucket = s3.Bucket(bucket_name)
+#     return bucket
 
 def local_cache(input_dir):
     for (dirpath, _, filenames) in os.walk(input_dir):
         for filename in filenames:
             if filename == "response_body":
                 with open(os.path.join(dirpath, filename)) as f:
-                    yield f.read()
+                    text = f.read()
+                    if "<title>Object moved</title>" in text:
+                        print(os.path.join(dirpath, filename))
+                        continue
+                    else:
+                        yield text
 
 def collect_data(items):
     for text in items:
-        # print(repr(text))
-        try:
-            data = json.loads(text)
-        except json.decoder.JSONDecodeError:
-            print(repr(data))
-            raise
+        data = json.loads(text)
+        if "error" in data:
+            continue
         if "EntitySets" in data['d']:
             continue
         items = data['d']['results']
         for item in items:
             yield item
 
-def write_data(data, f_name):
-    with open(f_name, "w") as f:
-        writeline = functools.partial(print, file=f)
-        for item in collect_data(data):
-            writeline(item)
+def write_data(data, output_dir):
+    for item in collect_data(data):
+        key = item['__metadata']['type'].split(".")[-1] + ".jsonlines"
+        filename = os.path.join(output_dir, key)
+        with open(filename, "a") as f:
+            writeline = functools.partial(print, file=f)
+            writeline(json.dumps(item))
 
 def main():
-    data = local_cache(settings.INPUT_DIR)
-    write_data(data, settings.OUTPUT_FILE)
-    # bucket = get_bucket()
-    # for text in get_objects(bucket):
+    data = local_cache(INPUT_DIR)
+    write_data(data, OUTPUT_DIR)
+
 
 if __name__ == '__main__':
     main()
